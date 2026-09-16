2660a861946cb968ee115cb583ffac042a390900
Your Name <youremail@students.uwf.edu>
2026-09-16 13:22:51 -0500

    Corrige incompatibilidade de char* no parseTokens

--- myshell.cpp
+++ myshell.cpp
@@ -1,33 +1,45 @@
-#include <iostream>
-#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include "param.hpp"
 #include "parse.hpp"
 
-using namespace std;
-
 int main(int argc, char const *argv[])
 {
-    string input;
     bool isDebugOn = false;
-    if(argc == 2) {
-        if(string(argv[1]) == "-Debug"){
-            isDebugOn = true;
-        }
+    if (argc == 2 && strcmp(argv[1], "-Debug") == 0) {
+        isDebugOn = true;
     }
-    while(true) {
+
+    char *line = NULL;
+    size_t bufferSize = 0;
+
+    while (true) {
         Param param;
-        cout << "$$$ ";
-        getline(cin, input);
-        if(input == "exit") {
+        printf("$$$ ");
+        fflush(stdout);
+
+        ssize_t charsRead = getline(&line, &bufferSize, stdin);
+        if (charsRead == -1) {
             break;
-        } else {
-            parseTokens(input.data(), &param);
         }
 
-        if(isDebugOn) {
+        size_t length = strlen(line);
+        if (length > 0 && line[length - 1] == '\n') {
+            line[length - 1] = '\0';
+        }
+
+        if (strcmp(line, "exit") == 0) {
+            break;
+        }
+
+        parseTokens(line, &param);
+
+        if (isDebugOn) {
             param.printParams();
         }
     }
 
+    free(line);
     return 0;
 }
